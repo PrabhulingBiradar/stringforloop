ddc4b42fd27b34610434c99ad0dae2c082aef274
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-22 22:47:16 +0530

    contactnum

---
+++ Contactnumber.java
@@ -0,0 +1,15 @@
+class Contactnumber
+{
+public static void main (String[] args)
+{
+String []numbers={"9954251156","5545846456","9954561515","9954255645","4564528796","5494949446", "5465584188","4564528796","5494949446", "5465584188"};
+for (int index=0; index<numbers.length;index=index+1)
+{
+
+System.out.println(numbers[index]);
+
+}
+
+}
+
+}
