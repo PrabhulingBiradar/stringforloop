3e02333fef25ec6651e5fdd27915372b23d88d36
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-22 22:48:29 +0530

    dayout

---
+++ Daysout.java
@@ -0,0 +1,11 @@
+class Daysout
+{
+	public static void main(String[] args)
+{
+		String []days = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+	
+{	System.out.println(days[0]+ "," +days[1]+ "," +days[2]+ "," + days[3]+ "," +days[4]+ "," + days[5]+ "," + days[6] + "," + days[7] + "," + days[8]);
+}
+	}
+
+}
