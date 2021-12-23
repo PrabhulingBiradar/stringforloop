fe566e27618795ef18b8fef162e68db61b0d8373
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-23 11:03:14 +0530

    types

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+class Datatypes
+{
+public static void main (String[] args)
+{
+String[] primitive={"byte","short","int","long","boolean","char","float","double"};
+
+ for(int index=0; index<=primitive.length;index=index+1)
+{
+
+System.out.println(primitive[index]);
+
+}
+
+}
+
+}
